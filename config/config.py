774d6a98b5c807370efb8a267da1b5ceba9032ee
Hamza khan <85861582+Hamzakhan7473@users.noreply.github.com>
2025-04-01 01:05:17 -0400

    Delete config directory

--- config/config.py
+++
@@ -1,9 +0,0 @@
-# config/config.py
-
-# NYC Crime Data API Configuration
-NYC_API_URL = ""
-
-# Twilio API Configuration (Optional for SMS alerts)
-TWILIO_ACCOUNT_SID = ""
-TWILIO_AUTH_TOKEN = ""
-TWILIO_PHONE_NUMBER = ""
